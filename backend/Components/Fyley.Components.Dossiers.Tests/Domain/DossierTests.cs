af2f9e58f76d1308b5234f0d7d548d3627c0ee77
Jeroen Verwimp <jeroen@verwimp.me>
2020-10-26 19:28:43 +0100

    Added tests for the dossiers domain

---
+++ backend/Components/Fyley.Components.Dossiers.Tests/Domain/DossierTests.cs
@@ -0,0 +1,33 @@
+﻿using DDDCore.Domain.Events;
+using Fyley.Components.Dossiers.Domain;
+using Fyley.Components.Dossiers.Domain.Events;
+using NUnit.Framework;
+
+namespace Fyley.Components.Dossiers.Tests.Domain
+{
+    [TestFixture]
+    public class DossierTests
+    {
+        public class ConstructorShould : DossierTests
+        {
+            [Test]
+            public void SetNameAndHaveDossierCreatedEvent()
+            {
+                // Arrange
+                var dossierName = new DossierName("dossier1");
+                
+                // Act
+                var result = new Dossier(dossierName);
+                
+                // Assert
+                Assert.That(result.Name, Is.EqualTo(dossierName));
+                
+                var events = result.FlushUncommitedEvents();
+                Assert.That(events, Has.Length.EqualTo(1));
+                Assert.That(events,
+                    Has.Exactly(1).Matches<DomainEvent>(@event =>
+                        (@event.AggregateEvent as DossierCreated)?.Name == dossierName));
+            }
+        }        
+    }
+}
