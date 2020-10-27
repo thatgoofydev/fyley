458d6dd095db14055aa457794e18f5b1494c5ece
Jeroen Verwimp <jeroen@verwimp.me>
2020-10-27 22:32:39 +0100

    Fixed loader spacing

--- frontend/packages/dossiers/example/index.tsx
+++ frontend/packages/dossiers/example/index.tsx
@@ -4,12 +4,12 @@ import { DossiersOverview } from "../src/DossiersOverview";
 
 const DossiersExamples: FunctionComponent = () => {
   return (
-    <>
-      <main>
-        <h1>Dossiers Example App</h1>
+    <main>
+      <h1>Dossiers Example App</h1>
+      <div className="example-wrapper">
         <DossiersOverview />
-      </main>
-    </>
+      </div>
+    </main>
   );
 };
 
