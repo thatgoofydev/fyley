b012629b7a6bd8bb23104beb37c04dd141f9d3d6
Jeroen Verwimp <jeroen@verwimp.me>
2020-10-28 19:13:30 +0100

    Changed loader spacing

--- frontend/packages/ui-lib/src/loader/Loader.tsx
+++ frontend/packages/ui-lib/src/loader/Loader.tsx
@@ -3,13 +3,11 @@ import "./Loader.scss";
 
 export const Loader: FunctionComponent = () => {
   return (
-    <div className="loader-wrapper">
-      <div className="lds-ellipsis">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    <div className="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
