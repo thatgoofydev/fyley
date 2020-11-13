8b13000f7d59bb0ac7f83cd9274f6ef312258d3e
Jeroen Verwimp <jeroen@verwimp.me>
2020-11-14 00:52:42 +0100

    Removed unused fronted project

--- frontend/packages/accounts/example/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from "react";
-import ReactDom from "react-dom";
-
-const Example: FC = () => {
-  return <main>accounts</main>;
-};
-
-ReactDom.render(<Example />, document.getElementById("root"));
--- frontend/packages/accounts/src/Example/Example.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from "react";
-import styles from "./Example.scss";
-
-export const Example: FC = () => {
-  return <h1 className={styles.example}>Example Text</h1>;
-};
