8e4d15c0431aa5eee897d8e0be930fcd00248bfb
Jeroen Verwimp <jeroen@verwimp.me>
2020-12-06 12:42:59 +0100

    Added basic Integration Test for account submit-form

---
+++ backend/BFF/Fyley.BFF.Desktop.IntegrationTests/Financial/Accounts/AccountControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Fyley.BFF.Desktop.Components.Financial.Accounts.WebApi.Models.Submit;
+using Fyley.BFF.Desktop.IntegrationTests.Helpers;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Fyley.BFF.Desktop.IntegrationTests.Financial.Accounts
+{
+    [TestFixture]
+    public class AccountControllerTests : IDisposable
+    {
+        private IntegrationTestFixture _fixture;
+        private HttpClient _http;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new IntegrationTestFixture();
+            _http = _fixture.GetClient();
+        }
+
+        [Test]
+        public async Task SubmitForm_ReturnsRandomId_WhenIdIsNew()
+        {
+            // Arrange
+            var request = new SubmitAccountRequest
+            {
+                Name = "Saving",
+                Description = "For saving some money",
+                AccountNumber = "BE34812896199790",
+                AccountNumberType = SubmitAccountRequest.AccountNumberTypes.Iban
+            };
+            
+            // Act
+            var response = await _http.PostJsonAsync("/bff/desktop/accounts/submit-form/new", request);
+
+            // Assert
+            Assert.That(response.IsSuccessStatusCode);
+
+            var apiResponse = await response.ReadApiResponseOf<SubmitAccountResponse>();
+            Assert.That(apiResponse.Ok, Is.True);
+            Assert.That(apiResponse.Error, Is.Null);
+            Assert.That(apiResponse.Data.Id, Is.Not.Null);
+        }
+
+        public void Dispose()
+        {
+            _fixture?.Dispose();
+            _http?.Dispose();
+        }
+    }
+}
