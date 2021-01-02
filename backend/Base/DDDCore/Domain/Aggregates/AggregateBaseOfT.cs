e18313dd12abf751b5167b87bfcdf34014378e0f
Jeroen Verwimp <jeroen@verwimp.me>
2021-01-02 23:27:05 +0100

    Removed commented out code

--- backend/Base/DDDCore/Domain/Aggregates/AggregateBaseOfT.cs
+++ backend/Base/DDDCore/Domain/Aggregates/AggregateBaseOfT.cs
@@ -10,7 +10,6 @@ namespace DDDCore.Domain.Aggregates
         where TIdentifier : Identifier
         where TState : class, IAggregateState
     {
-        private readonly Dictionary<Type, Delegate> _eventAppliers = new Dictionary<Type, Delegate>();
         private readonly Dictionary<Type, MethodInfo> _eventApplierMethods = new Dictionary<Type, MethodInfo>();
         private readonly ICollection<IAggregateEvent> _uncommitedEvents = new LinkedList<IAggregateEvent>();
         
@@ -63,7 +62,6 @@ namespace DDDCore.Domain.Aggregates
         private void ApplyEvent(IAggregateEvent @event)
         {
             _eventApplierMethods[@event.GetType()].Invoke(State, new object[] { @event });
-            // _eventAppliers[@event.GetType()]?.DynamicInvoke(@event);
         }
         
         private void RegisterEventAppliers()
@@ -77,10 +75,6 @@ namespace DDDCore.Domain.Aggregates
                 var aggregateEventType = emitInterface.GenericTypeArguments.First();
                 var applyMethod = emitInterface.GetMethod(nameof(IHandle<IAggregateEvent>.Apply));
                 _eventApplierMethods.Add(aggregateEventType, applyMethod);
-                
-                // var delegateType = typeof(Action<>).MakeGenericType(aggregateEventType);
-                // var action = Delegate.CreateDelegate(delegateType, State, applyMethod ?? throw new InvalidOperationException());
-                // _eventAppliers.Add(aggregateEventType, action);
             }
         }
     }
